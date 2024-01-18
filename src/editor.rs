d1c34ac322e198f96332ac89aa91886207170b24
Nerd Guy <nerdguy@Jenishkumars-MacBook-Pro.local>
2024-01-18 22:26:10 +0530

    Make editor with movable cursor and ~ lines

---
+++ src/editor.rs
@@ -0,0 +1,124 @@
+use crate::Terminal;
+use termion::event::Key;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+pub struct Position {
+    pub x: usize,
+    pub y: usize,
+}
+
+pub struct Editor {
+    should_quit: bool,
+    terminal: Terminal,
+    cusrsor_position: Position,
+}
+
+impl Editor {
+    pub fn default() -> Self {
+        Self {
+            should_quit: false,
+            terminal: Terminal::default().expect("Failed to initialize terminal"),
+            cusrsor_position: Position { x: 0, y: 0 },
+        }
+    }
+
+    pub fn run(&mut self) {
+        loop {
+            if let Err(err) = self.refresh_screen() {
+                die(err);
+            }
+            if self.should_quit {
+                break;
+            }
+            if let Err(err) = self.process_keypress() {
+                die(err);
+            }
+        }
+    }
+
+    fn process_keypress(&mut self) -> Result<(), std::io::Error> {
+        let pressed_key = Terminal::read_key()?;
+        match pressed_key {
+            Key::Ctrl('x') => self.should_quit = true,
+            Key::Up
+            | Key::Down
+            | Key::Left
+            | Key::Right
+            | Key::PageDown
+            | Key::PageUp
+            | Key::Home
+            | Key::End => self.move_cursor(pressed_key),
+            _ => (),
+        }
+        Ok(())
+    }
+
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        Terminal::cursor_hide();
+        Terminal::cursor_position(&Position { x: 0, y: 0 });
+        if self.should_quit {
+            Terminal::clear_screen();
+            println!("Goodbye.\r");
+        } else {
+            self.draw_rows();
+            Terminal::cursor_position(&self.cusrsor_position);
+        }
+        Terminal::cursor_show();
+        Terminal::flush()
+    }
+
+    fn draw_welcome_message(&self) {
+        let mut welcome_message = format!("Writer editor -- version {}", VERSION);
+        let width = self.terminal.size().width as usize;
+        let len = welcome_message.len();
+        let padding = width.saturating_sub(len) / 2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_message = format!("~{}{}", spaces, welcome_message);
+        welcome_message.truncate(width);
+        println!("{}\r", welcome_message);
+    }
+
+    fn draw_rows(&self) {
+        let height = self.terminal.size().height;
+        for row in 0..height {
+            Terminal::clear_current_line();
+            if row == height / 3 {
+                self.draw_welcome_message();
+            } else {
+                println!("~\r");
+            }
+        }
+    }
+
+    fn move_cursor(&mut self, key: Key) {
+        let Position { mut x, mut y } = self.cusrsor_position;
+        let size = self.terminal.size();
+        let height = size.height.saturating_sub(1) as usize;
+        let width = size.width.saturating_sub(1) as usize;
+        match key {
+            Key::Up => y = y.saturating_sub(1),
+            Key::Down => {
+                if y < height {
+                    y = y.saturating_add(1)
+                }
+            }
+            Key::Left => x = x.saturating_sub(1),
+            Key::Right => {
+                if x < width {
+                    x = x.saturating_add(1)
+                }
+            }
+            Key::Home => x = 0,
+            Key::PageDown => y = height,
+            Key::PageUp => y = 0,
+            Key::End => x = width,
+            _ => (),
+        }
+        self.cusrsor_position = Position { x, y };
+    }
+}
+
+fn die(err: std::io::Error) {
+    panic!("{}", err);
+}
