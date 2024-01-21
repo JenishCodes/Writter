5b2697b59b8d7e015d000c8e4d074e592e41441b
Nerd Guy <nerdguy@Jenishkumars-MacBook-Pro.local>
2024-01-21 17:35:19 +0530

    Added number highlighting

--- src/row.rs
+++ src/row.rs
@@ -1,4 +1,5 @@
 use std::cmp;
+use termion::color;
 use unicode_segmentation::UnicodeSegmentation;
 
 use crate::editor::SearchDirection;
@@ -30,10 +31,21 @@ impl Row {
             .skip(start)
             .take(end - start)
         {
-            if grapheme == "\t" {
-                result.push_str("  ");
-            } else {
-                result.push_str(grapheme);
+            if let Some(c) = grapheme.chars().next() {
+                if c == '\t' {
+                    result.push_str("  ");
+                } else if c.is_ascii_digit() {
+                    result.push_str(
+                        &format!(
+                            "{}{}{}",
+                            color::Fg(color::Rgb(220, 163, 163)),
+                            c,
+                            color::Fg(color::Reset)
+                        )[..],
+                    )
+                } else {
+                    result.push(c);
+                }
             }
         }
         result
